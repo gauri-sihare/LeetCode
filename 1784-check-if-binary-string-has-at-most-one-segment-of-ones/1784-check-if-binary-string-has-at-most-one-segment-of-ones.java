2f5e5280b63e7950b0eb76c2d34137b5cdce48aa
gauri sihare <gaurisihare787@gmail.com>
2026-03-06 12:43:35 +0530

    Time: 0 ms (100%), Space: 43.2 MB (5.09%) - LeetHub

---
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkOnesSegment(String s) {
+        if(s.length()==1 && s.charAt(0)==1){
+            return true;
+        }
+        
+        for(int i =s.length()-1 ; i>=1 ;i--){
+            if(s.charAt(i) == '1' && s.charAt(i-1) =='0'){
+                return false;
+            }
+            // return true;
+        }
+        return true;
+    }
+}
