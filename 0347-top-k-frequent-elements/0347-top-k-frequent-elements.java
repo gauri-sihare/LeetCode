ef8834f706eb4c4b4571d4f382e85295d1ababc4
gauri sihare <gaurisihare787@gmail.com>
2026-05-27 13:19:29 +0530

    Time: 15 ms (54.82%), Space: 47.5 MB (64.66%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -4,10 +4,10 @@ class Solution {
         PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> hm.get(b) - hm.get(a));
         // max heap.
         for(int i =0; i< nums.length ;i++){
-            hm.put(nums[i] , hm.getOrDefault(nums[i] , 0)+1);
+            hm.put(nums[i] , hm.getOrDefault(nums[i] , 0)+1);   //freq store.
 
         }
-        pq.addAll(hm.keySet());
+        pq.addAll(hm.keySet());    //ekbaar me sbko pq me daal do 
 
         int ans[] = new int[k];
         for(int i =0; i<k ;i++){
