08574767c88c9f456b38e38dadeec33fa8fcfe48
gauri sihare <gaurisihare787@gmail.com>
2026-07-10 11:39:15 +0530

    Time: 0 ms (100%), Space: 42.7 MB (85.79%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,15 +15,15 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null && q == null){
-            return true;
-        }
-        if( p == null || q == null){
+        if(p == null && q == null){     
+            return true ;
+        }else if(q==null || p== null){
             return false;
-        }
-        if(p.val != q.val){
+        }else if(p.val != q.val){
             return false;
-        }
-        return isSameTree(p.left , q.left) && isSameTree(p.right, q.right);
+        } 
+        
+        return isSameTree(p.right , q.right) &&
+        isSameTree(p.left , q.left);
     }
 }
